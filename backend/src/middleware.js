b765cce41b5ba1eba3ae14442d9c376d3df03350
Ankit vishwa <aviatankit@gmail.com>
2025-11-30 23:05:34 +0530

    Update middleware.js

--- backend/src/middleware.js
+++ backend/src/middleware.js
@@ -1,1 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+exports.userAuth = (req, res, next) => {
+  let token = req.headers["authorization"];
+
+  if (!token) return res.status(401).json({ message: "No Token Provided" });
+
+  try {
+    let data = jwt.verify(token, "SECRET123");
+    req.user = data;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid Token" });
+  }
+};
 
