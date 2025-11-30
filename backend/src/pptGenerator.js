6782a8a3cdb209d94bc5c6c24cf382388e5e0c04
Ankit vishwa <aviatankit@gmail.com>
2025-11-30 23:03:53 +0530

    Update pptGenerator.js

--- backend/src/pptGenerator.js
+++ backend/src/pptGenerator.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const PPTX = require("pptxgenjs");
+const router = express.Router();
+
+router.get("/download", async (req, res) => {
+  let ppt = new PPTX();
+
+  let slide = ppt.addSlide();
+  slide.addText("GetKhojo PPT Report", { x: 1, y: 1, fontSize: 32 });
+
+  let fileName = "getkhojo_report.pptx";
+
+  await ppt.writeFile({ fileName });
+
+  res.download(fileName);
+});
+
+module.exports = router;
 
